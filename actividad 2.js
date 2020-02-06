51366c1ce1fe4f60d1eee9652e3ecb5fe13e2e1c
Yeffry Aroche <yeffryss.deras@gmail.com>
2020-02-06 14:50:57 -0600

    subiendo archivos en github

---
+++ actividad 2.js
@@ -0,0 +1,31 @@
+
+var mes = 10;
+var numero = 20007024;
+var nombre = "Yeffry Armando Aroche Deras";
+
+alert("Bienvenido ");
+mes = prompt("ingrese el numero");
+
+if(mes == 12){
+  alert("es invierno");
+}else if (mes == 1){
+  alert("es invierno");
+}else if(mes== 2){
+  alert("es invierno");
+}else if(mes == 3){
+  alert("es primavera");
+}else if(mes == 5){
+  alert("es primavera");
+}else if(mes == 6){
+  alert("es verano");
+}else if(mes == 7){
+  alert("es verano");
+}else if(mes == 9){
+  alert("es otoño");
+}else if(mes == 11){
+  alert("es otoño");
+}else{
+  alert("el numero no es correcto");
+}
+alert('carnet es: ' +numero+ '  nombre: ' +nombre+'');
+
